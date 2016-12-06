a0d64e3ca844c173e2aa2c8f9ac7a7f529e64d28
Tiansi Dong <tdong@uni-bonn.de>
2016-12-06 10:53:40 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='graph2',
+      version='0.1',
+      description='graph of graph',
+      url='http://github.com/HimmelStein/graph2',
+      author='Tiansi Dong',
+      author_email='tian1shi2@gmail.com',
+      license='MIT',
+      packages=['graph2'],
+      zip_safe=False)
---
+++ tests/test_core.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from .context import graph2
+
+import json
+import unittest
+
+
+class TestGraph2(unittest.TestCase):
+    """Basic test cases."""
+
+    def test_en2de(self):
+       return True
+
+
+if __name__ == '__main__':
+    unittest.main()
